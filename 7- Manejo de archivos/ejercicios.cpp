4520f8b16b86463cc28d6857e6f3f6cf45d962ab
Pablo Bustos <pablobustos609@gmail.com>
2024-08-29 21:23:53 -0300

    add ejercicio2

--- 7- Manejo de archivos/ejercicios.cpp
+++ 7- Manejo de archivos/ejercicios.cpp
@@ -3,13 +3,29 @@
 
 using namespace std;
 
+void leerFloats(FILE *stream) {
+    float num;
+
+    fseek(stream, 0, SEEK_SET);
+
+    cout << "numeros: ";
+
+    while (fread(&num, sizeof(float), 1, stream)) {
+        cout << num << ", ";
+    }
+
+    cout << "\n" << endl;
+}
+
 void ejercicio1() {
+    cout << "********************* Ejercicio 1 *********************\n" << endl;
+
     int elementAmount, bytesAmount;
     float num;
     FILE *stream;
 
     if((stream = fopen("bin/ejercicio1", "wb+")) != NULL) {
-        cout << "Ingresar cantidad de números que va a ingresar: ";
+        cout << "Ingresar cantidad de numeros que va a ingresar: ";
         cin >> elementAmount;
 
         bytesAmount = elementAmount * sizeof(float);
@@ -21,13 +37,11 @@ void ejercicio1() {
             cin >> num;
 
             fwrite(&num, sizeof(float), 1, stream);
-            cout << "Se escribio " << sizeof(float) << " bytes\n" << endl;
         }
 
-        cout << "Cursor en byte " << ftell(stream) << endl;
         fseek(stream, -bytesAmount, SEEK_CUR);                                           // Si bytesAmount es 12, muevo el cursor 12 bytes hacia atrás
-        cout << "Cursor se movio y esta en byte " << ftell(stream) << "\n" << endl;
 
+        cout << "\n" << endl;
         cout << "Numeros guardados: ";
 
         for (int i = 0; i < elementAmount; i++) {
@@ -38,14 +52,50 @@ void ejercicio1() {
             if (i < elementAmount-1) cout << ", ";
         }
 
+        cout << "\n" << endl;
+
         fclose(stream);
     } else {
-        cout << "No se pudo abrir el archivo";
+        cout << "No se pudo abrir el archivo\n" << endl;
     }
 
 }
 
+void ejercicio2() {
+    cout << "********************* Ejercicio 2 *********************\n" << endl;
+
+    int byte;
+    float newNumber;
+    FILE *stream;
+
+    if ((stream = fopen("bin/ejercicio1", "rb+")) != NULL) {
+        do {
+            cout << "Ingrese byte donde se encuentra el numero a reemplazar: ";
+            cin >> byte;
+        } while(byte % 4 != 0);
+
+        cout << "Ingresar nuevo numero a guardar en esa posicion: ";
+        cin >> newNumber;
+
+        fseek(stream, byte, SEEK_SET);
+
+        cout << "\nCursor en posicion " << ftell(stream) << endl;
+
+        fwrite(&newNumber, sizeof(float), 1, stream);
+
+        cout << "Numero actualizado y cursor en posición " << ftell(stream) << "\n" << endl;
+
+        leerFloats(stream);
+
+        fclose(stream);
+    } else {
+        cout << "No se pudo abrir el archivo\n" << endl;
+    }
+}
+
 int main() {
     ejercicio1();
+    ejercicio2();
+
     return 0;
 }

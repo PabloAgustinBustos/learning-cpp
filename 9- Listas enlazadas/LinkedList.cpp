13c05be45b70e44a6cd9420633407c0df620537a
PabloAgustinBustos <pablobustos609@gmail.com>
2024-09-20 12:28:39 -0300

    add delete functions and prnitListValues

--- 9- Listas enlazadas/LinkedList.cpp
+++ 9- Listas enlazadas/LinkedList.cpp
@@ -18,12 +18,25 @@ void logNode(Node* node) {
     cout << "  next: " << node->next << endl;
     cout << "}" << endl;
 }
-void printList(Node* node) {
+void printListValues(Node* node) {
     while (node != NULL) {
-        logNode(node);
+        //logNode(node);
+        cout << node->value;
+
+        if (node->next == NULL) cout << "\n";
+        else cout << ", ";
+
         node = node->next;
     }
 }
+void printList(Node* node) {
+    while (node != NULL) {
+        logNode(node);
+
+        node = node->next;
+    }
+}
+
 void addFirst(Node* &head, int newNumber) {
     Node* aux = head;   // Mantengo guardado el head en otro puntero para no perderlo
 
@@ -50,6 +63,7 @@ void addLast(Node* &head, int newNumber) {
     last = last->next;          // Ahora hay un nuevo último nodo, lo obtengo.
     last->value = newNumber;
 }
+
 Node* getNode(Node* head, int number) {
     Node* node = head;
 
@@ -58,6 +72,42 @@ Node* getNode(Node* head, int number) {
     }
 
     return node;
+}
+
+void deleteFirst(Node* &head) {
+    if (head) {
+        Node *aux = head;
+
+        head = head->next;
+
+        delete aux;
+    }
+
+}
+void deleteLast(Node* &head) {
+    if (!head) return;  // Caso de lista vacía
+
+    Node* last = head;
+    Node* beforeLast;   // Necesito trackear también el nodo anterior al último
+
+    // Mientras que haya un siguiente nodo
+    while (last->next) {
+        beforeLast = last;
+        last = last->next;
+    }
+
+    if (last == head) {
+        // Caso de único elemento en la lista
+
+        delete head;    // Liberamos la memoría
+        head = NULL;    // Dejamos de apuntar a un espacio vacío
+    } else {
+        // Caso donde haya más elementos
+
+        beforeLast->next = NULL;    // Desconectamos los nodos
+        delete last;    // Eliminamos el que antes era el último
+        last = NULL;
+    }
 }
 
 int main() {
@@ -71,17 +121,12 @@ int main() {
     addFirst(head, -1);
 
     cout << "Lista: " << endl;
-    printList(head);
+    printListValues(head);
 
-    Node* elem = getNode(head, 2);
+    deleteLast(head);
 
-    cout << "\nEncontre este nodo ";
-    logNode(elem);
-
-    cout << "\n";
-
-    printList(head);
+    cout << "\nLista: " << endl;
+    printListValues(head);
 
     return 0;
 }
-
